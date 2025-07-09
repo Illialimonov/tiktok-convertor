38d998b23cf4b164e963d1a1a0b8446886831ee3
ilia_lymonov <ilialimits222@gmail.com>
2025-07-08 21:11:28 -0400

    your message

--- src/main/java/com/example/ilia/singlevideoconvertor1/MainController.java
+++ src/main/java/com/example/ilia/singlevideoconvertor1/MainController.java
@@ -160,9 +160,9 @@ public class MainController {
                         "[0:a]atrim=start=%d:end=%d,asetpts=PTS-STARTPTS[a]\" " +
                         "-map \"[v]\" -map \"[a]\" " +
                         "-c:v libx264 -preset ultrafast -crf 35 " +
-                        "-c:a aac -b:a 96k -movflags +faststart -f mp4 \"/home/ilialimits222/output/%s_chopped.mp4\"",
+                        "-c:a aac -b:a 96k -movflags +faststart -f mp4 \"%s_chopped.mp4\"",
                 dlStart, dlEnd,
-                format, youtubeUrl,
+                "worstvideo+bestaudio", youtubeUrl,
                 "minecraft",
                 ffmpegStart, ffmpegEnd,
                 timingList.get(0), timingList.get(1),
